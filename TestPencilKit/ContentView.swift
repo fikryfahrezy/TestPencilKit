b635c1ba9e8424de5febeff21849ca0a63ddafac
Fikry Fahrezy <ffahrezi69@gmail.com>
2025-03-08 17:14:42 +0700

    Refactor MyCanvasView to simplify layout handling and remove unused tool picker observer methods

--- TestPencilKit/ContentView.swift
+++ TestPencilKit/ContentView.swift
@@ -21,7 +21,6 @@ struct MyCanvasView: UIViewRepresentable {
         toolPicker.setVisible(true, forFirstResponder: canvasView)
         toolPicker.addObserver(canvasView)
         toolPicker.addObserver(context.coordinator)
-        context.coordinator.updateLayout(for: toolPicker)
         canvasView.becomeFirstResponder()
         
         return canvasView
@@ -72,62 +71,11 @@ struct MyCanvasView: UIViewRepresentable {
             )
             
             self.parent.canvasView.contentSize = CGSize(
-                width: DataModel.canvasWidth * self.parent.canvasView.zoomScale,
+                width: self.parent.canvasView.bounds.width,
                 height: contentHeight
             )
         }
-        
-        // MARK: Tool Picker Observer
-        
-        /// Delegate method: Note that the tool picker has changed which part of the canvas view
-        /// it obscures, if any.
-        func toolPickerFramesObscuredDidChange(_ toolPicker: PKToolPicker) {
-            updateLayout(for: toolPicker)
-        }
-        
-        /// Delegate method: Note that the tool picker has become visible or hidden.
-        func toolPickerVisibilityDidChange(_ toolPicker: PKToolPicker) {
-            updateLayout(for: toolPicker)
-        }
-        
-        /// Helper method to adjust the canvas view size when the tool picker changes which part
-        /// of the canvas view it obscures, if any.
-        ///
-        /// Note that the tool picker floats over the canvas in regular size classes, but docks to
-        /// the canvas in compact size classes, occupying a part of the screen that the canvas
-        /// could otherwise use.
-        func updateLayout(for toolPicker: PKToolPicker) {
-            let obscuredFrame = toolPicker.frameObscured(
-                in: self.parent.canvasView
-            )
-            
-            // If the tool picker is floating over the canvas, it also contains
-            // undo and redo buttons.
-            if obscuredFrame.isNull {
-                self.parent.canvasView.contentInset = .zero
-            }
-            
-            // Otherwise, the bottom of the canvas should be inset to the top of the
-            // tool picker, and the tool picker no longer displays its own undo and
-            // redo buttons.
-            else {
-                self.parent.canvasView.contentInset = UIEdgeInsets(
-                    top: 0,
-                    left: 0,
-                    bottom: self.parent.canvasView.bounds.maxY - obscuredFrame.minY,
-                    right: 0
-                )
-            }
-            self.parent.canvasView.scrollIndicatorInsets = self.parent.canvasView.contentInset
-        }
     }
-}
-
-/// `DataModel` contains the drawings that make up the data model, including multiple image drawings and a signature drawing.
-struct DataModel: Codable {
-    
-    /// The width used for drawing canvases.
-    static let canvasWidth: CGFloat = 768
 }
 
 #Preview {
